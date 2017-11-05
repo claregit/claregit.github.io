b55c9bbe01e20e4c7ceb126aed41bf8151b23622
Clare Burns <vcburns@ncsu.edu>
2017-11-05 00:33:04 -0400

    Updates

---
+++ build_site.R
@@ -0,0 +1,9 @@
+#install.packages("rmarkdown", type = "source")
+#library(rmarkdown)
+
+# set website working directory
+setwd('C:/Users/Clare/Documents/GitHub/claregit.github.io')
+getwd()
+
+# render website
+rmarkdown::render_site()
