203945be51c79779b1254b0ba915f2fa1f11c77b
claregit <virginiaclareburns@gmail.com>
2017-11-04 23:50:57 -0400

    Delete build_site.R

--- build_site.R
+++
@@ -1,8 +0,0 @@
-#install.packages("rmarkdown", type = "source")
-#library(rmarkdown)
-
-# set website working directory
-#setwd('C:/Users/Clare/Documents/GitHub/claregit.github.io')
-
-# render website
-rmarkdown::render_site()
